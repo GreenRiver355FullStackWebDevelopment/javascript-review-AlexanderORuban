e87ce2dd0c51dee8eadbc6ae02b6d85cb5373ce0
Alexander Ruban <alexruban29@gmail.com>
2025-04-06 16:32:29 -0700

    Finished first deliverable and added comments.

--- src/index.js
+++ src/index.js
@@ -23,3 +23,45 @@ const reviews = [
 /////////////////////////////////////////////////////////////////////
 
 //Your Code Below Here////
+const renderReview = (review) => {
+  // Creating an individual review container
+  const reviewContainer = document.createElement("div");
+  reviewContainer.className = "review_container";
+
+  // Creating image element from the image and adding it to the container
+  const image = document.createElement("img");
+  image.src = review.image;
+  reviewContainer.appendChild(image);
+
+  // Creating the internal div for storing information
+  const infoDiv = document.createElement("div");
+
+  // Creating and adding the username to the infoDiv container
+  const username = document.createElement("p");
+  username.textContent = review.username;
+  infoDiv.appendChild(username);
+
+  // Creating and adding the star rating to the infoDiv container
+  const starRating = document.createElement("p");
+  starRating.textContent = review.starRating;
+  infoDiv.appendChild(starRating);
+
+  // Creating and adding the review to the infoDiv container
+  const textReview = document.createElement("p");
+  textReview.textContent = review.review;
+  infoDiv.appendChild(textReview);
+
+  // Appending the infoDiv container inside the review container
+  reviewContainer.appendChild(infoDiv);
+
+  // Adding the review container to the reviews container
+  document.querySelector('.reviews').appendChild(reviewContainer);
+}
+
+// Rendering all reviews using the renderReview function
+const renderAllReviews = () => {
+  reviews.forEach(renderReview);
+}
+
+// Calling the render function
+renderAllReviews();
